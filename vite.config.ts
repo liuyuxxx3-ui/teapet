7c6bc84dc8789798d816a95a779269f40fa0f6d3
liuyuxxx3-ui <you@example.comliuyuxxx3@gmail.com>
2026-02-15 11:56:35 +0800

    fix: 配置路径别名 @，修复构建错误

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: 'all',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
